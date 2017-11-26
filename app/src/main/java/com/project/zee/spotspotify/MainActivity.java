675129e7b9ae7b376d7d41a66f0b8e0421d3867e
Zeeshan Sagheer Ansari <zeesagheer@gmail.com>
2017-11-26 22:12:11 +0530

    Changed Compatibility

--- app/src/main/java/com/project/zee/spotspotify/MainActivity.java
+++ app/src/main/java/com/project/zee/spotspotify/MainActivity.java
@@ -3,12 +3,12 @@ package com.project.zee.spotspotify;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/project/zee/spotspotify/TopTen.java
+++ app/src/main/java/com/project/zee/spotspotify/TopTen.java
@@ -2,12 +2,12 @@ package com.project.zee.spotspotify;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TopTen extends ActionBarActivity {
+public class TopTen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
